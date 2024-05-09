8ac5228f4369a27e467a08863999af0fa9fcd57e
Bruno Tetzner <brunoltetzner@gmail.com>
2024-05-08 21:07:30 -0300

    fix: remove edit donator data on representor-donation-screen

--- src/pages/donor-screen/script-modal.js
+++ src/pages/donor-screen/script-modal.js
@@ -76,8 +76,8 @@ buttonSubmitProfileChanges.addEventListener('click', function() {
   newProfile.bornAt = verifyAndReplace(inputEditBornAt, newProfile.bornAt)
   newProfile.bloodType = verifyAndReplace(inputEditBloodType, newProfile.bloodType)
   newProfile.city = verifyAndReplace(inputEditCity, newProfile.city)
-
-  if(newProfile.bornAt.includes('-')) {
+  console.log(newProfile)
+  if(newProfile.bornAt?.includes('-')) {
     const splitData = newProfile.bornAt.split('-');
     const formatedBornAt = splitData[2] + '/' + splitData[1] + '/' + splitData[0];
     newProfile.bornAt = formatedBornAt 
