162a2fc28f9a6d79d060232de44536c39d97cc95
Bruno Tetzner <brunoltetzner@gmail.com>
2024-05-09 20:59:08 -0300

    fix: persist user data on database

--- src/pages/representative-donor-screen/index.js
+++ src/pages/representative-donor-screen/index.js
@@ -1,15 +1,3 @@
-const deleteAccount = () => {
-  const userId = window.location.search.split('id=')[1].split('&')[0];
-  signOut();
-  deleteUser(userId);
-  window.location.assign('../representative-screen/index.html');
-};
-
-const editAccount = () => {
-  const userId = window.location.search.split('id=')[1].split('&')[0];
-  addEditAccountModalToDOM(userId)
-}
-
 const scheduleDonation = () => {
   const userId = window.location.search.split('id=')[1].split('&')[0];
   addScheduleDonationModalToDOM(userId)
@@ -18,16 +6,7 @@ const scheduleDonation = () => {
 
 document.addEventListener('DOMContentLoaded', () => {
   verifyRepresentativeUser();
-  
-  // Registrando função callback para o botão
-  const deleteAccountbutton = document.getElementById('delete-account-button');
-  deleteAccountbutton.addEventListener('click', () => {
-    addDeleteAccountModalToDOM(deleteAccount);
-  });
-
-  const editButton = document.getElementById('edit-account-button');
-  editButton.addEventListener('click', editAccount);
-  
+    
   const scheduleDonationButton = document.getElementById('open-modal-schedule-donation');
   scheduleDonationButton.addEventListener('click', scheduleDonation);
 });
